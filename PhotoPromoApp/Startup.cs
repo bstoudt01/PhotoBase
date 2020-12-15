bd44916c53984602fdb6f5e59208170ce4bca720
Brett Stoudt <bstoudt01@gmail.com>
2020-12-15 13:28:17 -0600

    removed unwanted s3 service tests from startup.cs, successfully created a bucket with a http post request using postman

--- PhotoPromoApp/Startup.cs
+++ PhotoPromoApp/Startup.cs
@@ -54,6 +54,8 @@ namespace PhotoPromoApp
                     };
                 });
             services.AddControllers();
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
